2a056b8b57467bed166f92ed83ff44e459362092
nikitavoitik <nikitavoitik2006@gmail.com>
2025-09-29 16:01:14 +0200

    add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +23,30 @@ export const metadata = {
   icons: {
     icon: '/favicon.png',
   },
-
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-EDGYEM2760"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-EDGYEM2760');
+            `,
+        }}
+      />
+    </head>
     <body
       className={`${geistSans.variable} ${geistMono.variable} ${spaceGrotesk.variable} antialiased`}
     >
